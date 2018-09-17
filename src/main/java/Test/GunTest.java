8a3db3270e02ee270bdc21dee0de40be6329b049
JAir Aguirre <aguirre.jair@gmail.com>
2018-09-16 22:26:30 -0500

    NodeListTes class was updated

--- src/main/java/Test/GunTest.java
+++ src/main/java/Test/GunTest.java
@@ -5,15 +5,63 @@ import static org.junit.Assert.*;
 import org.junit.AfterClass;
 import org.junit.Test;
 
-public class GunTest {
+import junit.framework.TestCase;
+import model.Gun;
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
+public class GunTest extends TestCase{
+
+	private String name;
+	private int bullets;
+	private Gun gun;
+	
+	void escenarioUno() {
+		
+		
+		name = "Shark";
+		bullets = 32;
+		
+		gun = new Gun(name, bullets);
+		
 	}
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetName() {
+		
+		escenarioUno();
+	
+		assertEquals(gun.getName(), name);
 	}
-
+	@Test
+	public void testSetName() {
+		
+		escenarioUno();
+		
+		String other = "Bird";
+			
+		gun.setName(other);
+		
+		assertEquals(gun.getName(), other);
+	
+	}
+	@Test
+	public void testGetBullets() {
+		
+		escenarioUno();
+		
+		int bullet = gun.getBullets();
+		assertTrue(bullets == bullet);
+	}
+	@Test
+	public void tesSettBullets() {
+		
+		escenarioUno();
+		int newbullets = 3;
+		gun.setBullets(newbullets);
+		
+		int updatedBullests = gun.getBullets();
+		assertTrue(newbullets != updatedBullests);
+		
+	}
+	
 }
